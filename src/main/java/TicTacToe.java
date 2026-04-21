31525aebd3be1027cff66969afa9f62ae51c3c62
Tanvi <atanvi928@gmail.com>
2026-04-21 13:44:33 +0530

    UC1 Added

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -1,23 +1,24 @@
 public class TicTacToe {
-
+    static char[][] board = new char[3][3];
     public static void main(String[] args) {
-        char[][] board = new char[3][3];
-
-        // Initialize board
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                board[i][j] = '-';
+        initializeBoard();   // fill board with '-'
+        printBoard();        // display board
+    }
+    static void initializeBoard() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                board[row][col] = '-';
             }
         }
-
-        // Print board
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j] + " ");
+    }
+    static void printBoard() {
+        System.out.println("-------------");
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                System.out.print("| " + board[row][col] + " ");
             }
-            System.out.println();
+            System.out.println("|");
+            System.out.println("-------------");
         }
     }
 }
-
-
