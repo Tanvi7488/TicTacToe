c46d9f3a4445164431f5ad03a887ff5b655c508e
Tanvi <atanvi928@gmail.com>
2026-05-06 13:42:19 +0530

    uc2

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -1,33 +1,34 @@
-import java.util.Scanner;
+import java.util.Random;
 
 public class TicTacToe {
-
-    public static int getUserSlot() {
-        Scanner scanner = new Scanner(System.in);
-        int slot;
-
-        while (true) {
-            System.out.print("Enter slot number (1-9): ");
-
-            if (scanner.hasNextInt()) {
-                slot = scanner.nextInt();
-
-                if (slot >= 1 && slot <= 9) {
-                    return slot;
-                }
-            } else {
-                scanner.next();
-            }
-
-            System.out.println("Invalid input. Try again.");
-        }
-    }
-
     public static void main(String[] args) {
 
+        Random rand = new Random();
+        int toss = rand.nextInt(2);
 
-        int userSlot = getUserSlot();
+        String player1 = "Player 1";
+        String player2 = "Player 2";
 
-        System.out.println("User selected slot: " + userSlot);
+        char player1Symbol, player2Symbol;
+        String currentPlayer;
+        char currentSymbol;
+
+        if (toss == 0) {
+            currentPlayer = player1;
+            player1Symbol = 'X';
+            player2Symbol = 'O';
+            currentSymbol = player1Symbol;
+        } else {
+            currentPlayer = player2;
+            player2Symbol = 'X';
+            player1Symbol = 'O';
+            currentSymbol = player2Symbol;
+        }
+
+        System.out.println("Toss Result: " + toss);
+        System.out.println(currentPlayer + " will start first.");
+        System.out.println(player1 + " symbol: " + player1Symbol);
+        System.out.println(player2 + " symbol: " + player2Symbol);
+        System.out.println("Current Player: " + currentPlayer + " (" + currentSymbol + ")");
     }
 }
